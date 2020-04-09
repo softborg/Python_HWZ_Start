fc394814ffaba01d1f4fb945719c2d87c1b30b0e
munot <stefan.berger@softborg.com>
2020-04-09 08:36:03 +0200

    csv beispiel ergänzt

--- kurstag_5/beispiele_5/datei_lesen.py
+++ kurstag_5/beispiele_5/datei_lesen.py
@@ -4,7 +4,6 @@ meine_datei = open("test.txt")  # Öffnet eine Datei im aktuellen Verzeichnis
 inhalt = meine_datei.read()  # liest die Datei aus
 print("Der Datei - Inhalt: " + inhalt)  # Ausgabe der Datei
 
-
 # Wäre die Datei nicht vorhanden, dann gäbe es einen Fehler
 # Darum ist es besser die Datei mit einer Fehlerbehandlung zu lesen
 
@@ -15,3 +14,21 @@ try:
 except FileNotFoundError:
     print("Datei {} konnte nicht gefunden werden".format(file_name))
 
+# Datei Zeile für Zeile einlesen
+try:
+    with open('test.txt', 'r') as dritte_datei:
+        for line in dritte_datei.readlines():
+            print("Datei lesen Zeile für Zeile: ", line)
+except FileNotFoundError:
+    print("Datei {} konnte nicht gefunden werden".format(file_name))
+
+# Datei Zeile für Zeile einlesen
+file_name = "files/kantone.csv"
+try:
+    with open(file_name, 'r') as dritte_datei:
+        for line in dritte_datei.readlines():
+            currentline = line.split(";")
+            for split in currentline:
+                print(split, end=' ')
+except FileNotFoundError:
+    print("Datei {} konnte nicht gefunden werden".format(file_name))
